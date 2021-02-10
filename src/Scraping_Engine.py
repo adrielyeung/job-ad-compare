44cc2d5375635e74df55d88ad9ff944a95264e58
unknown <tsunyeung1997@gmail.com>
2021-02-10 17:45:43 +0800

    Add page toggling for scraping more jobs

--- src/Scraping_Engine.py
+++ src/Scraping_Engine.py
@@ -25,10 +25,6 @@ class Scraping_Engine:
         
         location : string
             Filter location of job read from GUI (input from user).
-
-        Returns
-        -------
-        None.
         '''
         self._data = data
         self.category = category
@@ -40,6 +36,10 @@ class Scraping_Engine:
         self.location_list = []
         self.description_list = []
         self.link_list = []
+        self._first_page_title_list = []
+        self._first_page_company_list = []
+        self._previous_page_title_list = []
+        self._previous_page_company_list = []
     
     def scrape_all(self):
         '''
@@ -48,57 +48,129 @@ class Scraping_Engine:
         if not hasattr(self, '_driver'):
             self._driver = self._set_up_headless_driver()
         for index, row in self._data.iterrows():
-            self.scrape(row)
+            # Lists below are for checking when any repetitions occur, which means to stop scraping
+            # List of job titles of first page scraped
+            self._first_page_title_list = []
+            self._first_page_company_list = []
+            # List of job titles of previous page scraped
+            self._previous_page_title_list = []
+            self._previous_page_company_list = []
+            scrape_finish = False
+            
+            page_num = int(row['Page_num_start'])
+            while not scrape_finish:
+                scrape_finish = self.scrape(row, page_num)
+                page_num += int(row['Page_num_increment'])
     
-    def scrape(self, row):
+    def scrape(self, row, page_num=1):
         '''
         Scrape site for HTML tags as described in row self._row in the Pandas dataframe.
+        
+        Parameters
+        ----------
+        row : pandas.Series
+            A row of config data, contains info of 1 website for scraping
+        page_num : int, default 1
+            Page number to scrape (if the site supports page toggling via page number)
         '''
         self._row = row
+        if len(self._first_page_title_list) == 0:
+            first_page = True
+        else:
+            first_page = False
+        
         separator = self._row['Search_keyword_separator']
         category_URL = self._convert_to_URL(self.category, separator)
         location_URL = self._convert_to_URL(self.location, separator)
-        fetch_URL = self._row['Listing_URL'].format(job_query=category_URL, location_query=location_URL)
+        
+        # Check if website supports toggling page number
+        if '{page_num}' in self._row['Listing_URL']:
+            toggle_page = True
+        else:
+            toggle_page = False
+        
+        fetch_URL = self._row['Listing_URL'].format(job_query=category_URL, location_query=location_URL, page_num=page_num, previous_page_num=page_num-1)
+        print(fetch_URL)
         page = requests.get(fetch_URL)
+        # Check for redirections -> this means the page number does not exist
+        if toggle_page and len(page.history) > 0:
+            return True
         soup = BeautifulSoup(page.content, 'html.parser')
-        results = self._element_finder.find_item_by(self._row, soup, 'Result')
-        job_elems = self._element_finder.find_all_by(self._row, results, 'Element')
+        if soup is not None:
+            results = self._element_finder.find_item_by(self._row, soup, 'Result')
+            
+            if results is not None:
+                job_elems = self._element_finder.find_all_by(self._row, results, 'Element')
+                checked_stop = False
         
-        for job_elem in job_elems:
-            content = None
-            title_elem = self._element_finder.find_item_by(self._row, job_elem, 'Title')
-            company_elem = self._element_finder.find_item_by(self._row, job_elem, 'Company')
-            location_elem = self._element_finder.find_item_by(self._row, job_elem, 'Location')
-            URL = self._element_finder.find_item_by(self._row, job_elem, 'URL')
-            
-            if not any((title_elem, company_elem, location_elem, URL)):
-                continue
-            
-            URL = URL['href']
-
-            if str.startswith(URL, '/'):
-                URL = self._row['Prefix_URL'] + URL
-            
-            try:
-                self._driver.get(URL)
-                if self._row['Description_attribute'] == 'id':
-                    content = self._driver.find_element_by_id(self._row['Description_tag'])
-                elif self._row['Description_attribute'] == 'class':
-                    content = self._driver.find_element_by_class_name(self._row['Description_tag'])
-                elif self._row['Description_attribute'] == 'xpath':
-                    content = self._driver.find_element_by_xpath(self._row['Description_tag'])
-                if not content is None:
-                    self.description_list.append('\nDescription\n===========\n' + content.text + '\n')
-                else:
-                    self.description_list.append('\nDescription\n===========\nNot found\n')
-            except (KeyError, NoSuchElementException):
-                continue
+                for job_elem in job_elems:
+                    content = None
+                    title_elem = self._element_finder.find_item_by(self._row, job_elem, 'Title')
+                    company_elem = self._element_finder.find_item_by(self._row, job_elem, 'Company')
+                    location_elem = self._element_finder.find_item_by(self._row, job_elem, 'Location')
+                    URL = self._element_finder.find_item_by(self._row, job_elem, 'URL')
+                    
+                    if not any((title_elem, company_elem, location_elem, URL)):
+                        continue
+                    
+                    URL = URL['href']
+                    
+                    # Compare job result with first page and previous page to see if any redirections have occured
+                    # Compare job title and company
+                    # If yes, scraping is finished and should stop
+                    if not first_page and not checked_stop:
+                        for index in (i for i, e in enumerate(self._first_page_title_list) if e == self._print_format_unicode(title_elem)):
+                            if self._first_page_company_list[index] == self._print_format_unicode(company_elem):
+                                print("Stopped match with first page job " + self._print_format_unicode(title_elem) + " " + self._print_format_unicode(company_elem))
+                                return True
+                            
+                        for index in (i for i, e in enumerate(self._previous_page_title_list) if e == self._print_format_unicode(title_elem)):
+                            if self._previous_page_company_list[index] == self._print_format_unicode(company_elem):
+                                print("Stopped match with previous page job " + self._print_format_unicode(title_elem) + " " + self._print_format_unicode(company_elem))
+                                return True
+                        
+                    self._previous_page_title_list = []
+                    self._previous_page_company_list = []
         
-            self.title_list.append('Title: ' + self._print_format_unicode(title_elem))
-            self.company_list.append('Company: ' + self._print_format_unicode(company_elem))
-            self.location_list.append('Location: ' + self._print_format_unicode(location_elem))
-            self.site_list.append('Source: ' + self._row['Site'] + '\n')
-            self.link_list.append('Link: ' + URL + '\n')
+                    if str.startswith(URL, '/'):
+                        URL = self._row['Prefix_URL'] + URL
+                    
+                    try:
+                        self._driver.get(URL)
+                        if self._row['Description_attribute'] == 'id':
+                            content = self._driver.find_element_by_id(self._row['Description_tag'])
+                        elif self._row['Description_attribute'] == 'class':
+                            content = self._driver.find_element_by_class_name(self._row['Description_tag'])
+                        elif self._row['Description_attribute'] == 'xpath':
+                            content = self._driver.find_element_by_xpath(self._row['Description_tag'])
+                        if not content is None:
+                            self.description_list.append('\nDescription\n===========\n' + content.text + '\n')
+                        else:
+                            self.description_list.append('\nDescription\n===========\nNot found\n')
+                    except (KeyError, NoSuchElementException):
+                        continue
+                    
+                    self.title_list.append('Title: ' + self._print_format_unicode(title_elem))
+                    if first_page:
+                        self._first_page_title_list.append(self._print_format_unicode(title_elem))
+                    self._previous_page_title_list.append(self._print_format_unicode(title_elem))
+                    
+                    self.company_list.append('Company: ' + self._print_format_unicode(company_elem))
+                    if first_page:
+                        self._first_page_company_list.append(self._print_format_unicode(company_elem))
+                    self._previous_page_company_list.append(self._print_format_unicode(company_elem))
+                    
+                    self.location_list.append('Location: ' + self._print_format_unicode(location_elem))
+                    self.site_list.append('Source: ' + self._row['Site'] + '\n')
+                    self.link_list.append('Link: ' + URL + '\n')
+                    
+                    checked_stop = True
+                    
+        if toggle_page and len(self._previous_page_title_list) > 0:
+            return False
+        else:
+            print("Stopped no more job")
+            return True
     
     def _set_up_headless_driver(self):
         '''
